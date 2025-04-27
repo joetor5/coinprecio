07923c3931efc258851ef8b694da31d53da7a70e
Joel Torres <jt@joeltorres.org>
2025-04-27 10:31:48 -0500

    Add license header

--- coinprecio/exceptions.py
+++ coinprecio/exceptions.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Joel Torres
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or https://opensource.org/license/mit.
 
 class CoinApiFetchError(Exception):
     pass
